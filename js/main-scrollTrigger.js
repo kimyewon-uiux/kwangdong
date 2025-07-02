be4c4879fde8d7ff8361139f7eb15738d9c122d2
kimyewon <kimyewon2020@gmail.com>
2025-07-02 17:53:09 +0900

    third commit

--- js/main-scrollTrigger.js
+++ js/main-scrollTrigger.js
@@ -18,4 +18,47 @@ productWrap
     .from('.product-wrap .product-swiper ul',{opacity : 0})
 
 
+const rndWrap = gsap.timeline({
+    scrollTrigger : {
+    trigger: '.rnd-wrap',
+    start: 'top top',
+    end : 'bottom 20%',
+    toggleActions : 'play none none reverse',
+    // scrub : true, 
+    // markers: true,
+    pin: true
+    }
+})
 
+rndWrap
+    .from('.rnd-wrap',{yPercent : +70})
+    .from('.rnd-wrap hgroup h2',{yPercent : -30, opacity : 0})
+    .from('.rnd-wrap hgroup p',{yPercent : -30, opacity : 0})
+    .from('.rnd-wrap .more-btn',{yPercent : -30, opacity : 0})
+    
+
+
+const historyWrap = gsap.timeline({
+    duration: 2,
+    scrollTrigger : {
+    trigger: '.history-wrap',
+    start: 'top top',
+    end : 'bottom 20%',
+    toggleActions : 'play none none reverse',
+    // scrub : true, 
+    markers: true,
+    pin: true
+    }
+})
+
+historyWrap
+    .from('.history-wrap .box1',{yPercent : +70,opacity : 0})
+    .to('.history-wrap .box1',{yPercent : -10,background:'#F0F0F0'})
+    .from('.history-wrap .box2',{yPercent : +70,opacity : 0})
+    .to('.history-wrap .box1',{yPercent : -20,background:'#F0F0F0'})
+    .to('.history-wrap .box2',{yPercent : -10,background:'#FDFDFD'})
+    .from('.history-wrap .box3',{yPercent : +70,opacity : 0})
+    .to('.history-wrap .box1',{yPercent : -30,background:'#EBEBEB'})
+    .to('.history-wrap .box2',{yPercent : -20,background:'#F0F0F0'})
+    .to('.history-wrap .box3',{yPercent : -10,background:'#FDFDFD'})
+    .from('.history-wrap .box4',{yPercent : +70,opacity : 0})

14e699bfdce8d0f042d452f8695d1ee4593ca6e6
kimyewon <kimyewon2020@gmail.com>
2025-08-01 13:15:47 +0900

    0801

--- js/main.js
+++ js/main.js
@@ -211,20 +211,14 @@ rndLiImg.forEach(tab => {
 
 // ESG
 const esgLi = document.querySelectorAll(".esg-card");
+
 esgLi.forEach((btn) => {
-    esgLi.addEventListener("click", () => {
-      const faqItem = esgLi.parentNode;
-      const isActive = btn.classList.contains("active");
-  
-      removeActiveClasses();
-  
-      if (!isActive) {
-        faqItem.classList.add("active");
-      }
-    });
-  });
-function removeActiveClasses() {
-esgLi.forEach((btn) => {
-    btn.classList.remove("active");
-});
-}
+    btn.addEventListener('click',()=>{
+        btn.classList.toggle("active")
+
+    if(!btn){
+        btn.classList.remove("active")
+    }    
+    })
+    
+})
